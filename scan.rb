0b39f76f5ca013cfb01e5dd726513b7740d60928
ironzombi <dr_grep@ironzombi.com>
2025-08-04 16:25:44 +0800

    port_open?(ip, port, timeout)

--- scan.rb
+++ scan.rb
@@ -5,6 +5,15 @@
 require 'socket'
 require 'timeout'
 
-def port_knock(ip, port, timeout = 2)
-
+def port_open?(ip, port, timeout = 2)
+  Timeout.timeout(timeout) do
+    begin
+      TCPSocket.new(ip, port).close
+      true
+    rescue Errno:ECONNREFUSED, Errno::EHOSTUNREACH, SocketError
+      false
+    end
+  end
+rescue Timeout::Error
+  false
 end
