7effa623ffd3cebbfbf8dbd06d9d3397ebecbfbd
ironzombi <span@ironzombi.com>
2025-08-05 08:49:06 +0800

    added scan_subnet

--- run.rb
+++ run.rb
@@ -1,7 +1,18 @@
 #!/usr/bin/env ruby
+require 'ipaddr'
 require_relative 'scan'
 
-ip = "165.118.134.135"
-port = 22
+subnet = "168.118.134.129/29"
+port 22
 
-puts(port_open?(ip, port))
+def scan_subnet(subnet, port)
+  IPAddr.new(subnet).to_range.each do |ip|
+    next if ip.to_s.end_with?('.0') || ip.to_s.end_with?('.255') # skip network/broadcast
+    print "Scanning #{ip}:#{port}... "
+    if port_open?(ip.to_s, port)
+      puts "OPEN"
+    else
+      puts "closed"
+    end
+  end
+end
