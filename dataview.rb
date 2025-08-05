d18b9e3a59632869a8b63a4c22cb10cfbdfbc50e
ironzombi <dr_grep@ironzombi.com>
2025-08-05 12:40:05 +0800

    group the ip's into /24's

---
+++ dataview.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/ruby
+require 'ipaddr'
+
+def group_ips_from_file(filename, subnet_mask = 24)
+  subnet_groups = Hash.new { |h, k| h[k] = [] }
+
+  File.readlines(filename).map(&:strip).uniq.each do |line|
+    begin
+      ip = IPAddr.new(line)
+      subnet = IPAddr.new("#{ip.to_s}/#{subnet_mask}").mask(subnet_mask).to_s + "/#{subnet_mask}"
+      subnet_groups[subnet] << ip.to_s
+    rescue IPAddr::InvalidAddressError
+      warn "Invalid IP address: #{line}"
+    end
+  end
+
+  subnet_groups
+end
+
+# Path to your flat IP file
+file_path = "ips.txt"
+grouped = group_ips_from_file(file_path)
+
+# Output grouped IPs
+grouped.each do |subnet, ips|
+  puts "Subnet #{subnet}:"
+  ips.sort.each { |ip| puts "  #{ip}" }
+end
+
