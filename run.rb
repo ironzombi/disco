f9138d4d067b8898d1ae7b5056e1816f9373a969
ironzombi <dr_grep@ironzombi.com>
2025-08-05 09:12:00 +0800

    write results to file

--- run.rb
+++ run.rb
@@ -3,18 +3,25 @@ require 'ipaddr'
 require_relative 'scan'
 
 subnet = "168.118.134.129/29"
-port 22
+port = 22
+FP = File.open("results.log", "w+")
 
 def scan_subnet(subnet, port)
   IPAddr.new(subnet).to_range.each do |ip|
     next if ip.to_s.end_with?('.0') || ip.to_s.end_with?('.255') # skip network/broadcast
     print "Scanning #{ip}:#{port}... "
     if port_open?(ip.to_s, port)
-      puts "OPEN"
+      FP.puts "#{ip} OPEN"
     else
       puts "closed"
     end
   end
 end
 
-scan_subnet(subnet, port)
+begin
+  scan_subnet(subnet, port)
+rescue Interrupt
+  puts ":"
+  puts "Stopping Scan"
+  exit
+end
